9deaa73ad82c4844fabf6743b430eb885a2aea84
rohit-tiwari12 <rt476502@gmail.com>
2026-04-25 02:52:15 +0530

    Updated frontend API URL for production

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_BASE_URL = "http://127.0.0.1:8000"
+API_BASE_URL = "https://newsmind-ai-backend.onrender.com"
 
 # ---------------- PAGE CONFIG ----------------
 st.set_page_config(
